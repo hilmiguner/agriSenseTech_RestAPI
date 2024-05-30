d1fb69916ff7ed43479f006765ac92c6c4fc3c0b
Hilmi Güner <hilmi.guner@hotmail.com>
2024-05-31 02:34:07 +0300

    Deleting weed data from database and deleting image from server feature has been added. Now every REST request from client is creates new database connection and close the connection when REST connection is finished for fetching the updated data in database.

--- database.py
+++ database.py
@@ -1,16 +1,20 @@
 import mysql.connector
 from hidden_constants import databaseConnectionInformation
 
-mydb = mysql.connector.connect(**databaseConnectionInformation)
-if mydb.is_connected():
-    print("Connection successfull")
-else:
-    print("Connection failure!!")
-
-cursor = mydb.cursor()
+def connectDatabase():
+    mydb = mysql.connector.connect(**databaseConnectionInformation)
+    if mydb.is_connected():
+        cursor = mydb.cursor()
+        print("Connection successfull")
+        return mydb, cursor
+    else:
+        print("Connection failure!!")
+        return -1, -1
 
 def insertUserMessage(title, message, name, email, datetime, fb_local_id):
     try:
+        mydb, cursor = connectDatabase()
+
         query = f"""
             INSERT INTO user_messages(title, message, name, email, datetime, fb_local_id) 
             VALUES ("{title}", "{message}", "{name}", "{email}", "{datetime}", "{fb_local_id}");
@@ -20,9 +24,13 @@ def insertUserMessage(title, message, name, email, datetime, fb_local_id):
         return { "status": 200 }
     except Exception as err:
         return { "status": 400, "error": str(err) }
+    finally:
+        mydb.close()
     
 def getWeeds(fb_local_id):
     try:
+        mydb, cursor = connectDatabase()
+
         query = f"""
             SELECT * FROM weeds WHERE fb_local_id='{fb_local_id}';
         """
@@ -42,3 +50,22 @@ def getWeeds(fb_local_id):
         return { "status": 200, "data": dataDict }
     except Exception as err:
         return { "status": 400, "error": str(err) }
+    finally:
+        mydb.close()
+    
+def deleteWeed(weedID):
+    try:
+        mydb, cursor = connectDatabase()
+
+        query = f"""
+            DELETE FROM weeds WHERE id='{weedID}';
+        """
+        cursor.execute(query)
+        
+        mydb.commit()
+
+        return { "status": 200, "data": "Deleting is successful." }
+    except Exception as err:
+        return { "status": 400, "error": str(err) }
+    finally:
+        mydb.close()
--- rest.py
+++ rest.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify 
 from hidden_constants import serverIP
-from database import insertUserMessage, getWeeds
+from database import insertUserMessage, getWeeds, deleteWeed
 import base64
+import os
 
 app = Flask(__name__)
 
@@ -27,15 +28,26 @@ def insert_user_message():
 @app.route("/get_weeds", methods=["POST"])
 def get_weeds():
     data = request.json
-
     try:
-        respond = getWeeds(data["fb_local_id"],)
+        respond = getWeeds(data["fb_local_id"])
         for id in respond["data"]:
             filename = respond["data"][id]["image_path"]
             with open(f"img_weeds/{data['fb_local_id']}/{filename}", "rb") as file:
                 imageData = file.read()
                 imageBase64 = base64.b64encode(imageData).decode("utf-8")
             respond["data"][id]["image"] = imageBase64
+        return jsonify(respond)
+    except Exception as err:
+        return jsonify({ 'status': 400, 'error': str(err) })
+    
+@app.route("/delete_weed", methods=["POST"])
+def delete_weed():
+    data = request.json
+
+    try:
+        respond = deleteWeed(data["id"])
+        if respond["status"] == 200:
+            os.remove("img_weeds/" + data["image_path"])
         return jsonify(respond)
     except Exception as err:
         return jsonify({ 'status': 400, 'error': str(err) })
